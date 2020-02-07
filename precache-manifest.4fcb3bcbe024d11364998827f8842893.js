e18e70d872cba97abaa34a1688561d14bc00bba4
Nazim Shala <shalanazim@gmail.com>
2020-02-07 15:40:56 +0100

    Updates

---
+++ precache-manifest.4fcb3bcbe024d11364998827f8842893.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4333ea9c1ddef76015fb15cc12ed0aff",
+    "url": "https///Hahiii.github.io//monsters-rolodex/index.html"
+  },
+  {
+    "revision": "e9c1310ae9d811b66204",
+    "url": "https///Hahiii.github.io//monsters-rolodex/static/css/main.b95f0374.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "https///Hahiii.github.io//monsters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "https///Hahiii.github.io//monsters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9c1310ae9d811b66204",
+    "url": "https///Hahiii.github.io//monsters-rolodex/static/js/main.b50c0ee3.chunk.js"
+  },
+  {
+    "revision": "6838423e67a828bd1c22",
+    "url": "https///Hahiii.github.io//monsters-rolodex/static/js/runtime-main.9f6ff9cb.js"
+  }
+]);
